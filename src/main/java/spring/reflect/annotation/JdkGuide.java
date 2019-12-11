d3912d7778032325f2eb47e270f99709b98ccf02
BigPayno <1361098889@qq.com>
2019-12-11 11:47:16 +0800

    Jdk Annotation Plus=>Spring Annotation

---
+++ src/main/java/spring/reflect/annotation/JdkGuide.java
@@ -0,0 +1,22 @@
+package spring.reflect.annotation;
+
+import org.springframework.core.annotation.AnnotatedElementUtils;
+
+/**
+ * @author payno
+ * @date 2019/12/11 11:44
+ * @description
+ */
+@EsbSer
+public class JdkGuide {
+    /**
+     * 看，jdk的注解根本不支持类似集成的机制。。。
+     * 所以Spring引入了AnnotationUtils和AnnotatedElementUtils
+     */
+    public static void main(String[] args) {
+       Com com=JdkGuide.class.getAnnotation(Com.class);
+       System.out.println(com);
+       Com com2= AnnotatedElementUtils.getMergedAnnotation(JdkGuide.class,Com.class);
+       System.out.println(com2);
+    }
+}
