48fb6ae92b87805d99625c463133d7ba9e0e361c
BigPayno <1361098889@qq.com>
2019-12-30 16:22:52 +0800

    真的讨厌Ecplise IDE maven用起来真坑

---
+++ src/main/java/idea/EcpliseGuide.java
@@ -0,0 +1,17 @@
+package idea;
+
+/**
+ * @author payno
+ * @date 2019/12/30 16:20
+ * @description
+ */
+public class EcpliseGuide {
+    /**
+     * 真的坑
+     * Ecplise的Apt插件使用
+     * 必须满足以下条件
+     * 1.Compiler模块必须配置好jdk版本及compiler插件版本
+     * 2.关闭自动build
+     * 3.clean->install
+     */
+}
