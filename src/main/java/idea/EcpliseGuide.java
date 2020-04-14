d10dc9cc7202f799007115341c55f14732c9fb4b
BigPayno <1361098889@qq.com>
2020-04-14 16:53:41 +0800

    ecplise

--- src/main/java/idea/EcpliseGuide.java
+++ src/main/java/idea/EcpliseGuide.java
@@ -17,4 +17,11 @@ public class EcpliseGuide {
      * ex:必须更新maven.pom（maven/update），否则默认执行缓存配置（猜测，导致Q类始终无法使用，即使包和classes文件夹都已经生成）
      *      而且project/clean会清除class中的Q.class
      */
+
+    /**
+     * 吐了
+     * 集成环境不需要配置环境变量
+     * 但是ecplise.ini启动配置必须是jdk
+     * 然后集成环境配置好其他配置即可
+     */
 }
